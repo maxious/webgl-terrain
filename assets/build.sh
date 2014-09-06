b38cbaccedfde0208065db1504a826ada127db99
Alex Sadleir <maxious@lambdacomplex.org>
2014-09-06 17:08:39 +1000

    up resolution, change export format

--- assets/build.sh
+++ assets/build.sh
@@ -7,9 +7,12 @@ gdal_translate -scale -98 1898 0 65535 -ot UInt16 -outsize 200 200 -of ENVI canb
 gdalbuildvrt canberra-s1s.vrt e*dems*
 #gdalwarp canberra-s1s.vrt canberra-s1s.tif
 # http://gis.stackexchange.com/questions/1755/how-to-resample-a-batch-of-rasters-using-ogr-gdal
+gdalwarp canberra-s1s.vrt canberra-s1s.tif
 # original size 10800
-gdalwarp -ts 1600 0 -r cubic -co "TFW=YES" canberra-s1s.vrt canberra-s1s.tif
+#gdalwarp -ts 1600 0 -r cubic -co "TFW=YES" canberra-s1s.vrt canberra-s1s.tif
 gdalinfo -mm canberra-s1s.tif
-gdal_translate -scale -86 1898 0 255 -outsize 200 200 -of PNG canberra-s1s.tif canberra-s1s.png
-gdal_translate -scale -86 1898 0 65535 -ot UInt16 -outsize 200 200 -of ENVI canberra-s1s.tif canberra-s1s.bin
+#gdal_translate -scale -86 1898 0 255 -outsize 200 200 -of PNG canberra-s1s.tif canberra-s1s.png
+#gdal_translate -scale -86 1898 0 65535 -ot UInt16 -outsize 200 200 -of ENVI canberra-s1s.tif canberra-s1s.bin
+gdal_translate -scale -86 1898 0 255 -outsize 2700 2700 -of PNG canberra-s1s.tif canberra-s1s.png
+gdal_translate -scale -86 1898 0 2000 -ot UInt16 -outsize 675 675 -of ENVI canberra-s1s.tif canberra-s1s.bin
 
--- assets/read.py
+++ assets/read.py
@@ -1,16 +1,39 @@
 import sys
 from struct import *
+from stl_tools import numpy2stl
+from stl_tools.cwrapped import tessellate #optional but faster
+from numpy  import *
+import os
+
+width = int(sys.argv[1])
+print "width: %d" % width
+#height = int(sys.argv[2])
+height = 65535
+print "max height: %d" % height
+
+filesize = os.path.getsize("canberra-s1s.bin")
 
 with open("canberra-s1s.bin", "rb") as f:
-    x = 0
-    y = 0
+    a = []
+    y = []
+    bytes_read = 0
     word = f.read(2)
     while word != "":
 	raw = float(unpack('h',word)[0])
-	value = abs(raw /65535*100)
-	print '%d,%d,%f' % (x,y,value)
-	x = x + 1
-	if x >= 200:
-	    y = y + 1
-	    x = 0
+	value = abs(raw /height * 100)
+	
+	#print '%d,%d,%f' % (x,y,value)
+	y.append(value)
+
+	if len(y) >= width:
+	    a.append(y)
+	    y = []
         word = f.read(2)
+	bytes_read = bytes_read + 2
+	if floor(bytes_read / 10000) % 10 == 0:
+		sys.stdout.write("\r %d / %d (%f)" % (bytes_read, filesize, ((bytes_read/filesize)*100 )))
+		sys.stdout.flush()
+
+A=array(a)
+
+numpy2stl(A, "out.stl", scale=0.5, solid=False)
